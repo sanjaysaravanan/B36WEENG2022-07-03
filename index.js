d180e92de955ddc9ebb8ea9d3a5f0c598eefcf4a
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2022-07-03 17:12:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+// const audioElement = document.getElementById('msg-sound');
+
+// const playSound = () => {
+//   audioElement.play();
+// }
+
+// const audioElement = document.getElementById('sample-inp');
+
+
+// console.log(document);
+
+
+const testDiv = document.getElementById('test-div');
+
+console.log(testDiv);
+
+// Set Attribute
+// Class to a div
+// testDiv.setAttribute('class', 'border-col bg-primary');
+
+// Classlist
+testDiv.classList.add('border-col');
+
+
+// Innertext
+testDiv.innerText = "<h1>Something</h1>";
+testDiv.style.color = 'white';
+testDiv.style.backgroundColor = 'blue';
+
+testDiv.title = "Sample";
+
+
+// innerHtml
+// testDiv.innerHTML = "<h1>Something</h1>";
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
